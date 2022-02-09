0f363238fac4861e99d3aaf3d88275b5408cdb58
unknown <elhadrinadia219@gmail.com>
2022-02-09 10:38:40 +0100

    modified in controller

--- controllers/employersControllers.php
+++ controllers/employersControllers.php
@@ -1,34 +1,31 @@
 <?php
 
-class EmployersControllers{
-    public  function getAllEmployes(){
-        // getAll function to get all elements from employes (static)
-        $employes = Employe::getAll();
-        return $employes;
-    }
-    public function addEmploye(){
-        if(isset($_POST['submit'])){
-            $data = array(
-                  'nom' => $_POST['nom'],  
-                  'prenom' => $_POST['prenom'],  
-                  'matricule' => $_POST['matricule'],  
-                  'poste' => $_POST['poste'],  
-                  'departement' => $_POST['departement'],  
-                  'date_embauche' => $_POST['date_embauche'], 
-                  'statut' => $_POST['statut'] 
-            );
-            $result = Employe::add($data);
-            if($result === 'ok'){
+class EmployersControllers
+{
+    // public function getAllEmployes()
+    // {
+    //     // getAll function to get all elements from employes (static)
+    //     $employes = Employe::getAll();
+    //     return $employes;
+    // }
+    // public function addEmploye()
+    // {
+    //     if (isset($_POST['submit'])) {
+    //         $data = array(
+    //             'nom' => $_POST['nom'],
+    //             'prenom' => $_POST['prenom'],
+    //             'matricule' => $_POST['matricule'],
+    //             'poste' => $_POST['poste'],
+    //             'departement' => $_POST['departement'],
+    //             'date_embauche' => $_POST['date_embauche'],
+    //             'statut' => $_POST['statut'],
+    //         );
+    //         $result = Employe::add($data);
+    //         if ($result === 'ok') {
 
-            }else{
-                echo $result;
-            }
-        }
-    }
+    //         } else {
+    //             echo $result;
+    //         }
+    //     }
+    // }
 }
-
-
-
-
-
-?>
